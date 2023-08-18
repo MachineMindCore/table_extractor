298c281326b8e092ce71d343dcfaae601c6cf5fd
MachineMindCore <machinemindcore@gmail.com>
2023-08-18 14:14:37 -0500

    fix get_last_directory

--- table_extractor/helpers.py
+++ table_extractor/helpers.py
@@ -1,4 +1,6 @@
 import os 
+import unicodedata
+import re
 
 def get_last_directory(url: str) -> str:
     """
@@ -10,8 +12,13 @@ def get_last_directory(url: str) -> str:
     pieces = url.split('/')
     if pieces[-1] == '':
         pieces.pop(-1)
+
+    normalized_name = unicodedata.normalize('NFKD', pieces[-1])
+    normalized_name = re.sub(r'[^a-zA-Z0-9 ]', '', normalized_name)
+    normalized_name = normalized_name.replace(' ', '_')
+    normalized_name = normalized_name.lower()
     
-    return pieces[-1]
+    return normalized_name
 
 def make_dir(base: str, dir: str):
     """
